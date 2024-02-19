f7028f8f2a4eb7124a81791a9e06ae48c430c7ac
David Huang <david.huang3214@gmail.com>
2024-02-19 14:16:14 -0800

    Fix fontset bug and cleanup code

--- platform.cpp
+++ platform.cpp
@@ -3,17 +3,14 @@
 Platform::Platform(char const* title, int windowWidth, int windowHeight, int textureWidth, int textureHeight){
 	SDL_Init(SDL_INIT_VIDEO);
 	
-	// Window to display front buffer
 	window = SDL_CreateWindow(
 		title, 
 		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 
 		windowWidth, windowHeight, 
 		SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
-	
-	// Front buffer
+
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 	
-	// Back buffer
 	texture = SDL_CreateTexture(
 		renderer, 
 		SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING,
